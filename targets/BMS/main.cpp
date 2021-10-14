86b9a106a6aa833dec96f46f71416b0fcb9c9c74
Collin Bolles <collinbolles@gmail.com>
2021-10-13 20:11:18 -0400

    Remove placeholder name

--- targets/BMS/main.cpp
+++ targets/BMS/main.cpp
@@ -3,140 +3,26 @@
  * basic echo functionality where the uart will write back whatever the user
  * enters.
  */
+#include <EVT/io/UART.hpp>
 #include <EVT/io/manager.hpp>
 #include <EVT/io/pin.hpp>
-#include <EVT/io/UART.hpp>
-#include <EVT/io/types/CANMessage.hpp>
-#include <EVT/utils/types/FixedQueue.hpp>
-#include <EVT/io/CANopen.hpp>
-#include <EVT/dev/platform/f3xx/f302x8/Timerf302x8.hpp>
-#include <EVT/dev/storage/EEPROM.hpp>
-#include <EVT/dev/storage/platform/M24C32.hpp>
-#include <BMS/BMSLogger.hpp>
-
-#include <BMS/BMS.hpp>
 
 namespace IO = EVT::core::IO;
-namespace DEV = EVT::core::DEV;
-namespace time = EVT::core::time;
-
-/**
- * Interrupt handler for incoming CAN messages.
- *
- * @param priv[in] The private data (FixedQueue<CANOPEN_QUEUE_SIZE, CANMessage>)
- */
-void canInterruptHandler(IO::CANMessage& message, void* priv) {
-    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>* queue =
-        (EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage>*)priv;
-    if (queue == nullptr)
-        return;
-    if (!message.isCANExtended())
-        queue->append(message);
-}
-
-///////////////////////////////////////////////////////////////////////////////
-// CANopen specific Callbacks. Need to be defined in some location
-///////////////////////////////////////////////////////////////////////////////
-extern "C" void CONodeFatalError(void) {
-    BMS::LOGGER.log(BMS::BMSLogger::LogLevel::ERROR, "Fatal CANopen error");
-}
-
-extern "C" void COIfCanReceive(CO_IF_FRM *frm) { }
-
-extern "C" int16_t COLssStore(uint32_t baudrate, uint8_t nodeId) { return 0; }
-
-extern "C" int16_t COLssLoad(uint32_t *baudrate, uint8_t *nodeId) { return 0; }
-
-extern "C" void CONmtModeChange(CO_NMT *nmt, CO_MODE mode) { }
-
-extern "C" void CONmtHbConsEvent(CO_NMT *nmt, uint8_t nodeId) { }
-
-extern "C" void CONmtHbConsChange(CO_NMT *nmt, uint8_t nodeId, CO_MODE mode) { }
-
-extern "C" int16_t COParaDefault(CO_PARA *pg) { return 0; }
-
-extern "C" void COPdoTransmit(CO_IF_FRM *frm) { }
-
-extern "C" int16_t COPdoReceive(CO_IF_FRM *frm) { return 0; }
-
-extern "C" void COPdoSyncUpdate(CO_RPDO *pdo) { }
-
-extern "C" void COTmrLock(void) { }
-
-extern "C" void COTmrUnlock(void) { }
 
 int main() {
     // Initialize system
     IO::init();
 
-    // Queue that will store CANopen messages
-    EVT::core::types::FixedQueue<CANOPEN_QUEUE_SIZE, IO::CANMessage> canOpenQueue;
-
-    // Initialize CAN, add an IRQ that will populate the above queue
-    IO::CAN& can = IO::getCAN<IO::Pin::PA_12, IO::Pin::PA_11>();
-    can.addIRQHandler(canInterruptHandler, reinterpret_cast<void*>(&canOpenQueue));
-
-    // Initialize the timer
-    DEV::Timerf302x8 timer(TIM2, 100);
-
+    // Setup UART
     IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
-    BMS::LOGGER.setUART(&uart);
-    BMS::LOGGER.setLogLevel(BMS::BMSLogger::LogLevel::DEBUG);
 
-    EVT::core::IO::I2C& i2c = EVT::core::IO::getI2C<IO::Pin::PB_8, IO::Pin::PB_9>();
-    EVT::core::DEV::M24C32 eeprom(0x50, i2c);
-
-    BMS::BQSettingsStorage bqSettingsStorage(eeprom);
-    BMS::BMS bms(bqSettingsStorage);
-
-    // Reserved memory for CANopen stack usage
-    uint8_t sdoBuffer[1][CO_SDO_BUF_BYTE];
-    CO_TMR_MEM appTmrMem[4];
-
-    // Initialize the CANopen drivers
-    CO_IF_DRV canStackDriver;
-
-    CO_IF_CAN_DRV canDriver;
-    CO_IF_TIMER_DRV timerDriver;
-    CO_IF_NVM_DRV nvmDriver;
-
-    IO::getCANopenCANDriver(&can, &canOpenQueue, &canDriver);
-    IO::getCANopenTimerDriver(&timer, &timerDriver);
-    IO::getCANopenNVMDriver(&nvmDriver);
-
-    canStackDriver.Can = &canDriver;
-    canStackDriver.Timer = &timerDriver;
-    canStackDriver.Nvm = &nvmDriver;
-
-    CO_NODE_SPEC canSpec = {
-        .NodeId = BMS::BMS::NODE_ID,
-        .Baudrate = IO::CAN::DEFAULT_BAUD,
-        .Dict = bms.getObjectDictionary(),
-        .DictLen = bms.getObjectDictionarySize(),
-        .EmcyCode = NULL,
-        .TmrMem = appTmrMem,
-        .TmrNum = 16,
-        .TmrFreq = 100,
-        .Drv = &canStackDriver,
-        .SdoBuf = reinterpret_cast<uint8_t*>(&sdoBuffer[0]),
-    };
-
-    CO_NODE canNode;
-
-    CONodeInit(&canNode, &canSpec);
-    CONodeStart(&canNode);
-    CONmtSetMode(&canNode.Nmt, CO_OPERATIONAL);
-
-    time::wait(500);
+    // String to store user input
+    char buf[100];
 
     while (1) {
-        // Process incoming CAN messages
-        CONodeProcess(&canNode);
-        // Update the state of timer based events
-        COTmrService(&canNode.Tmr);
-        // Handle executing timer events that have elapsed
-        COTmrProcess(&canNode.Tmr);
-        // Wait for new data to come in
-        time::wait(10);
+        // Read user input
+        uart.printf("Enter message: ");
+        uart.gets(buf, 100);
+        uart.printf("\n\recho: %s\n\r", buf);
     }
 }
