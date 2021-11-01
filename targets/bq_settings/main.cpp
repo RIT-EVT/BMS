821d1c6a35214d8be9aabe46c1062b94e73d091b
cbolles <collinbolles@gmail.com>
2021-11-01 18:23:39 -0400

    Create 'logger' for the BMS

--- targets/bq_settings/main.cpp
+++ targets/bq_settings/main.cpp
@@ -1,8 +1,10 @@
 #include <EVT/io/manager.hpp>
+#include <BMS/BMSLogger.hpp>
 
 namespace IO = EVT::core::IO;
 
 int main() {
     IO::UART& uart = IO::getUART<IO::Pin::UART_TX, IO::Pin::UART_RX>(9600);
+    BMS::LOGGER.setUART(&uart);
 
 }
