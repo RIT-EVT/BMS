6e9cb704243a92b3a228dc0e47ec5f584aa1249f
Collin Bolles <collinbolles@gmail.com>
2021-11-13 01:26:41 -0500

    BQ Settings Test
    Add settings to ensure BQ settings can be serialized and deserialized
    properly.

--- src/BQSetting.cpp
+++ src/BQSetting.cpp
@@ -1,9 +1,62 @@
 #include <BMS/BQSetting.hpp>
+#include <BMS/BMSLogger.hpp>
 
 namespace BMS {
 
-BQSetting::fromArray(uint8_t buffer[ARRAY_SIZE]) {
-    address = buffer
+BQSetting::BQSetting(BQSettingType settingType, uint8_t numBytes, uint16_t address, uint32_t data) :
+    settingType(settingType), address(address), data(data), numBytes(numBytes) {
+
+}
+
+BQSetting::BQSetting() {
+}
+
+void BQSetting::fromArray(uint8_t buffer[ARRAY_SIZE]) {
+    settingType = static_cast<BQSettingType>(buffer[0] & 0x3);
+    numBytes = (buffer[0] >> 2) & 0x7;
+
+    address = (static_cast<uint16_t>(buffer[2]) << 8) | buffer[1];
+
+    data = (static_cast<uint32_t>(buffer[6]) << 24)
+           | (static_cast<uint32_t>(buffer[5]) << 16)
+           | (static_cast<uint32_t>(buffer[4]) << 8)
+           | static_cast<uint32_t>(buffer[3]);
+
+    LOGGER.log(BMSLogger::LogLevel::DEBUG, "Setting Type: %u", settingType);
+    LOGGER.log(BMSLogger::LogLevel::DEBUG, "Address: 0x%4x", address);
+    LOGGER.log(BMSLogger::LogLevel::DEBUG, "Data:0x%8x", data);
+}
+
+void BQSetting::toArray(uint8_t buffer[ARRAY_SIZE]) {
+    // Command byte
+    uint8_t commandByte = static_cast<uint8_t>(settingType) | (numBytes << 2);
+    buffer[0] = commandByte;
+
+    // Address
+    buffer[1] = address & 0xFF;
+    buffer[2] = address >> 8;
+
+    // Data
+    buffer[3] = data & 0xFF;
+    buffer[4] = (data >> 8) & 0xFF;
+    buffer[5] = (data >> 16) & 0xFF;
+    buffer[6] = (data >> 24) & 0xFF;
+}
+
+BQSetting::BQSettingType BQSetting::getSettingType() {
+    return settingType;
+}
+
+uint16_t BQSetting::getAddress() {
+    return address;
+}
+
+uint32_t BQSetting::getData() {
+    return data;
+}
+
+uint8_t BQSetting::getNumBytes() {
+    return numBytes;
 }
 
 }  // namespace BMS
