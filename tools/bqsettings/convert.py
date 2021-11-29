2c11516736f2069a921a85543888294faf93675d
Collin Bolles <collinbolles@gmail.com>
2021-11-28 20:10:31 -0500

    Conversion logic, untested

--- tools/bqsettings/convert.py
+++ tools/bqsettings/convert.py
@@ -9,6 +9,79 @@ format which is a CSV of just the settings.
 import argparse
 import os
 import sys
+from common import BQSetting
+import pathlib
+from typing import List
+
+
+def load_from_ti(file_path: str) -> List[BQSetting]:
+    """
+    Load a list of the BQSettings from a TI file. Note, the TI file itself
+    only contains RAM settings so all settings will be of type RAM.
+
+    :param file_path: Path to the TI file to parse.
+    :return: List of parsed BQSettings
+    """
+    settings = []
+    with open(file_path, 'r') as ti_file:
+        for line in ti_file:
+            # Check for, and ignore, comments
+            if line[0] == '*':
+                continue
+            settings.append(BQSetting.from_ti(line))
+    return settings
+
+
+def load_from_csv(file_path: str) -> List[BQSetting]:
+    """
+    Load a list of the BQSettings from a CSV file.
+
+    :param file_path: Path to the CSV file to parse
+    :return: List of the parsed BQSettings
+    """
+    settings = []
+    with open(file_path, 'r') as csv_file:
+        for line in csv_file:
+            # Check for, and ignore, comments
+            if line[0] == '*':
+                continue
+            settings.append(BQSetting.from_csv(line))
+    return settings
+
+
+def save_to_csv(file_path: str, settings: List[BQSetting]) -> None:
+    """
+    Save the given settings into a CSV.
+    """
+    with open(file_path, 'w') as csv_file:
+        # Write out header
+        csv_file.write('Setting Type,Number of Bytes,Address,Data\n')
+        for setting in settings:
+            csv_file.write(setting.to_csv + '\n')
+
+
+def save_to_binary(file_path: str, settings: List[BQSetting]) -> None:
+    """
+    Save the provided settings into a binary format. The data will have all
+    the settings listed back-to-back. See TODO: Add URL for more details
+
+    :param file_path: The path to the file to write out the binary data
+    :param settings: The list of settings to convert
+    """
+    with open(file_path, 'wb') as binary_file:
+        for setting in settings:
+            binary_file.write(setting.to_binary)
+
+
+def is_ti_file(file_path: str) -> bool:
+    """
+    Return true if the given file path is for a TI settings file, TI
+    settings files should be names with ".gg.csv" as the extension
+
+    :param file_path: The file to check the type of
+    :return: True if the file has the extension ".gg.csv"
+    """
+    return pathlib.Path(file_path).name.endswith('.gg.csv')
 
 
 def convert_to_binary(args: argparse.Namespace):
@@ -17,8 +90,12 @@ def convert_to_binary(args: argparse.Namespace):
 
     See the `convert` function for the expected arguments within args.
     """
-    with open(args.input, 'r') as input_file:
-        print(type(input_file))
+    settings = []
+    if is_ti_file(args.input):
+        settings = load_from_ti(args.input)
+    else:
+        settings = load_from_csv(args.input)
+    save_to_binary(settings)
 
 
 def convert_to_csv(args: argparse.Namespace):
@@ -27,7 +104,12 @@ def convert_to_csv(args: argparse.Namespace):
 
     See the `convert` function for the expected arguments with args.
     """
-    pass
+    settings = []
+    if is_ti_file(args.input):
+        settings = load_from_ti(args.input)
+    else:
+        return
+    save_to_csv(settings)
 
 
 def convert(args: argparse.Namespace):
