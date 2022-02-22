43e0fe4dd3f5953c237ebb74d6399fc34963dc91
cbolles <collinbolles@gmail.com>
2022-02-21 19:32:03 -0500

    Begin working on I2C test interface over serial

--- src/dev/BQ76952.cpp
+++ src/dev/BQ76952.cpp
@@ -2,13 +2,14 @@
 
 namespace BMS::DEV {
 
-BQ76952::BQ76952(EVT::core::IO::I2C& i2c, uint8_t i2cAddress) : i2c(i2c), i2cAddress(i2cAddress) {
+BQ76952::BQ76952(EVT::core::IO::I2C& i2c, uint8_t i2cAddress)
+    : i2c(i2c), i2cAddress(i2cAddress) {
     // Empty constructor
 }
 
 void BQ76952::writeSetting(BMS::BQSetting& setting) {
     // Call the cooresponding setting write command
-    switch(setting.getSettingType()) {
+    switch (setting.getSettingType()) {
         case BMS::BQSetting::BQSettingType::DIRECT:
             this->writeDirectSetting(setting);
             break;
@@ -26,7 +27,7 @@ void BQ76952::enterConfigUpdateMode() {
     // config update mode
     // static constexpr uint8_t NUM_ATTEMPTS = 10;
 
-    uint8_t transfer[] = { 0x90, 0x00 };
+    uint8_t transfer[] = {0x90, 0x00};
     i2c.writeMemReg(i2cAddress, 0x3E, transfer, 2, 1, 100);
 
     // TODO: Wait for the 0x12 Battery Status()[CFGUPDATE] flag to set
@@ -39,8 +40,27 @@ void BQ76952::enterConfigUpdateMode() {
 }
 
 void BQ76952::exitConfigUpdateMode() {
-    uint8_t transfer[] = { 0x90, 0x00 };
+    uint8_t transfer[] = {0x90, 0x00};
     i2c.writeMemReg(i2cAddress, 0x3E, transfer, 2, 1, 100);
+}
+
+BQ76952::BQ76952Status BQ76952::makeDirectRead(uint8_t reg,
+                                               uint8_t* result) {
+
+    // Write out the target register
+    auto status = i2c.write(i2cAddress, reg);
+    if (status != EVT::core::IO::I2C::I2CStatus::OK) {
+        return BQ76952Status::ERROR;
+    }
+
+    // Attempt to read back the value
+    status = i2c.read(i2cAddress, result);
+    if (status != EVT::core::IO::I2C::I2CStatus::OK) {
+        return BQ76952Status::ERROR;
+    }
+
+    // Return successful
+    return BQ76952Status::OK;
 }
 
 void BQ76952::writeDirectSetting(BMS::BQSetting& setting) {
@@ -52,9 +72,7 @@ void BQ76952::writeDirectSetting(BMS::BQSetting& setting) {
     makeDirectCommand(reg, data);
 }
 
-void BQ76952::writeSubcommandSetting(BMS::BQSetting& setting) {
-
-}
+void BQ76952::writeSubcommandSetting(BMS::BQSetting& setting) {}
 
 void BQ76952::writeRAMSetting(BMS::BQSetting& setting) {
     // Array which stores all bytes that make up a RAM write request
@@ -68,22 +86,24 @@ void BQ76952::writeRAMSetting(BMS::BQSetting& setting) {
     transfer[0] = static_cast<uint8_t>(setting.getAddress() & 0xFF);
     transfer[1] = static_cast<uint8_t>((setting.getAddress() >> 8) & 0xFF);
 
-    for(int i = 0; i < setting.getNumBytes(); i++) {
+    for (int i = 0; i < setting.getNumBytes(); i++) {
         transfer[2 + i] = (setting.getData() >> (i * 8)) & 0xFF;
     }
 
     // i2c.write(i2cAddress, transfer, 3 + setting.getNumBytes());
-    i2c.writeMemReg(i2cAddress, RAM_BASE_ADDRESS, transfer, 2 + setting.getNumBytes(), 1, 100);
+    i2c.writeMemReg(i2cAddress, RAM_BASE_ADDRESS, transfer,
+                    2 + setting.getNumBytes(), 1, 100);
 
     // Calculate and write out checksum and data length,
     // checksum algorithm = ~(ram_address + sum(data_bytes))
     // Detailed in BQ76952 Software Development Guide
     uint8_t checksum = 0;
-    for(int i = 1; i < (3 + setting.getNumBytes()); i++) {
+    for (int i = 1; i < (3 + setting.getNumBytes()); i++) {
         checksum += transfer[i];
     }
     checksum = ~checksum;
-    uint8_t length = 1 + 3 + setting.getNumBytes(); // Extra 1 for the I2C address itself
+    uint8_t length =
+        1 + 3 + setting.getNumBytes();  // Extra 1 for the I2C address itself
 
     // transfer[0] = RAM_CHECKSUM_ADDRESS;
     transfer[0] = checksum;
@@ -97,7 +117,8 @@ void BQ76952::writeRAMSetting(BMS::BQSetting& setting) {
 void BQ76952::makeDirectCommand(uint8_t registerAddr, uint16_t data) {
     uint8_t* reg = &registerAddr;
     // Data in little endian
-    uint8_t bytes[] = { static_cast<uint8_t>(data & 0xFF), static_cast<uint8_t>(data >> 8) };
+    uint8_t bytes[] = {static_cast<uint8_t>(data & 0xFF),
+                       static_cast<uint8_t>(data >> 8)};
 
     i2c.writeReg(i2cAddress, reg, 1, bytes, 2);
 }
