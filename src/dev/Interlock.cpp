887ac618611f2f467d2a023c9db58fe3fbb01648
cbolles <collinbolles@gmail.com>
2022-03-16 15:52:27 -0400

    Add checking for interlock detect in charing and power delivery states

--- src/dev/Interlock.cpp
+++ src/dev/Interlock.cpp
@@ -5,6 +5,7 @@ namespace BMS::DEV {
 Interlock::Interlock(EVT::core::IO::GPIO& gpio) : gpio(gpio) {}
 
 bool Interlock::isDetected() {
+    // TODO: Use debounce for handling potential noise based issues
     return gpio.readPin() == ACTIVE_STATE;
 }
 
