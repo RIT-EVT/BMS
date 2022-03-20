4790aaa3a549ca521fbfdf09f84c9649fb6d31da
Collin Bolles <collinbolles@gmail.com>
2022-03-19 21:36:01 -0400

    Add command line arguments to python state test scripts

--- tools/status/check_status.py
+++ tools/status/check_status.py
@@ -1,11 +1,31 @@
+"""
+Simple script that checks the status of the BMS. This includes checking the
+current state of the state machine
+"""
 import canopen
+from argparse import ArgumentParser
 
-network = canopen.Network()
-network.connect(channel='/dev/ttyACM1', bustype='slcan')
 
-node = network.add_node(5, None)
-client = node.sdo
+def main():
+    argparser = ArgumentParser(description='''Utility to get the current state
+                               of the BMS''')
+    argparser.add_argument('port', action='store', type=str, help='''The
+                           port of the can device to interface with the
+                           BMS''')
+    argparser.add_argument('node', action='store', type=int, help='''The
+                           CANopen node ID of the BMS''')
+    args = argparser.parse_args()
 
-print(client.upload(0x2102, 0x0))
+    network = canopen.Network()
+    network.connect(channel=args.port, bustype='slcan')
 
-network.disconnect()
+    node = network.add_node(args.node, None)
+    client = node.sdo
+
+    print(client.upload(0x2102, 0x0))
+
+    network.disconnect()
+
+
+if __name__ == '__main__':
+    main()
--- tools/status/reset_status.py
+++ tools/status/reset_status.py
@@ -1,12 +1,35 @@
+"""
+Utility to reset the state of the BMS.
+"""
 import canopen
 import struct
+from argparse import ArgumentParser
 
-network = canopen.Network()
-network.connect(channel='/dev/ttyACM1', bustype='slcan')
 
-node = network.add_node(5, None)
-client = node.sdo
+def main():
+    argparser = ArgumentParser(description='''Utility to reset the state of
+                               the BMS''')
+    argparser.add_argument('port', action='store', type=str, help='''The
+                           port of the can device to interface with the
+                           BMS''')
+    argparser.add_argument('node', action='store', type=int, help='''The
+                           CANopen node ID of the BMS''')
+    argparser.add_argument('state', action='store', type=int, help='''The
+                           target state of the BMS. See `BMS::State` in
+                           `BMS.hpp` for the states and their corresponding
+                           numbers''')
+    args = argparser.parse_args()
 
-print(client.download(0x2102, 0x0, struct.pack('<I', 0x0)))
+    network = canopen.Network()
+    network.connect(channel=args.port, bustype='slcan')
 
-network.disconnect()
+    node = network.add_node(args.node, None)
+    client = node.sdo
+
+    client.download(0x2102, 0x0, struct.pack('<I', args.state))
+
+    network.disconnect()
+
+
+if __name__ == '__main__':
+    main()
