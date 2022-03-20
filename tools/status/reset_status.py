6232458f73dad2aa8541d3e747879a50aca355ff
Collin Bolles <collinbolles@gmail.com>
2022-03-19 21:24:08 -0400

    Working factory init logic

--- tools/status/reset_status.py
+++ tools/status/reset_status.py
@@ -2,11 +2,11 @@ import canopen
 import struct
 
 network = canopen.Network()
-network.connect(channel='/dev/cu.usbmodem143201', bustype='slcan')
+network.connect(channel='/dev/ttyACM1', bustype='slcan')
 
 node = network.add_node(5, None)
 client = node.sdo
 
-print(client.download(0x2102, 0x0, struct.pack('<I', 0x4)))
+print(client.download(0x2102, 0x0, struct.pack('<I', 0x0)))
 
 network.disconnect()
