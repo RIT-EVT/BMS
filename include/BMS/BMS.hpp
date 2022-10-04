3df706aac06217c3523ed32b318d8143cd77cbaa
Aaron Chan <aarchan108@gmail.com>
2022-10-03 20:50:58 -0400

    better keys

--- include/BMS/BMS.hpp
+++ include/BMS/BMS.hpp
@@ -348,7 +348,7 @@ private:
         {
             .Key = CO_KEY(0x1800, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
-            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0),
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(1),
         },
         {
             .Key = CO_KEY(0x1800, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
@@ -525,7 +525,7 @@ private:
         {
             .Key = CO_KEY(0x1800, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
-            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(0),
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(2),
         },
         {
             .Key = CO_KEY(0x1800, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
@@ -557,7 +557,7 @@ private:
         {
             .Key = CO_KEY(0x1801, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
-            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(1),
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(2),
         },
         {
             .Key = CO_KEY(0x1801, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
@@ -702,7 +702,7 @@ private:
         {
             .Key = CO_KEY(0x1800, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
-            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(1),
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(3),
         },
         {
             .Key = CO_KEY(0x1800, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
@@ -734,7 +734,7 @@ private:
         {
             .Key = CO_KEY(0x1801, 1, CO_UNSIGNED32 | CO_OBJ_D__R_),
             .Type = nullptr,
-            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(1),
+            .Data = (uintptr_t) CO_COBID_TPDO_DEFAULT(3),
         },
         {
             .Key = CO_KEY(0x1801, 2, CO_UNSIGNED8 | CO_OBJ_D__R_),
