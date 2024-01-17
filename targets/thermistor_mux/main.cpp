d6cf632aa030d29afbc5caa85637e546e22a4141
mjmagee991 <54601318+mjmagee991@users.noreply.github.com>
2024-01-17 08:23:10 -0500

    Cleaned Up Target thermistor_mux

--- targets/thermistor_mux/main.cpp
+++ targets/thermistor_mux/main.cpp
@@ -2,7 +2,6 @@
 * Tests the
 */
 
-#include <EVT/dev/Thermistor.hpp>
 #include <EVT/io/ADC.hpp>
 #include <EVT/io/GPIO.hpp>
 #include <EVT/manager.hpp>
@@ -14,16 +13,6 @@
 namespace IO = EVT::core::IO;
 
 namespace time = EVT::core::time;
-
-void switchMux(IO::GPIO* muxArr[3], uint8_t inputNum) {
-    for (uint8_t i = 0; i < 3; i++) {
-        muxArr[i]->writePin(inputNum & 1 << i ? IO::GPIO::State::HIGH : IO::GPIO::State::LOW);
-    }
-}
-
-uint32_t convert(uint32_t voltage) {
-    return voltage;
-}
 
 int main() {
     EVT::core::platform::init();
@@ -37,16 +26,16 @@ int main() {
 
     IO::GPIO* muxPinArr[3] = {&muxs1, &muxs2, &muxs3};
 
-    BMS::DEV::ThermistorMux tmux(muxPinArr, adc);
+    BMS::DEV::ThermistorMux thermistorMux(muxPinArr, adc);
 
     uint8_t looper = 0;
 
-    uart.printf("Starting ADC Voltage Testing -----\r\n");
+    uart.printf("Starting Thermistor Mux Testing -----\r\n");
 
     time::wait(500);
 
     while (1) {
-        uart.printf("%d\r\n", tmux.getTemp(looper));
+        uart.printf("%d\r\n", thermistorMux.getTemp(looper));
 
         looper = (looper + 1) % 8;
         if (looper == 0) {
